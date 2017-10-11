89eae377da24a2f345251d02a4f377093e744a1f
xbc <1158839560@qq.com>
2017-10-11 23:47:44 +0800

    user

--- scud/src/components/user.vue
+++ scud/src/components/user.vue
@@ -1,5 +1,18 @@
 <template>
-  <i class="icon">用户</i>
+  <div>
+    <div class="header">
+      <div class="basic-info"></div>
+      <div class="balance-info">
+        <div class="coupons"></div>
+        <div class="balance"></div>
+        <div class="recharge"></div>
+      </div>
+    </div>
+    <div class="record">
+      <div class="order">我的订单</div>
+      <div class="receipt">收货记录</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,7 +25,41 @@
   html{
     font-size: 16px;
   }
-  .icon{
-    font-size: 3rem;
+  .header{
+    width: 100%;
+    height: 520px;
+    background-color: #fff;
+    position: relative;
+  }
+  .basic-info{
+    width: 240px;
+    height: 240px;
+    position: absolute;
+    top: 40px;
+    left: 50%;
+    margin-left: -120px;
+    background-color: #aaa;
+  }
+  .balance-info{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    width: 100%;
+    height: 200px;
+  }
+  .balance-info div{
+    flex: 1;
+    margin: 3px;
+    background-color: #bbb;
+  }
+  .record{
+    width: 100%;
+    height: 120px;
+    margin-top: 40px;
+  }
+  .record div{
+    height: 60px;
+    background-color: #999;
   }
 </style>
